54aab2f9160946cd163f47f39a1d6d183c21a841
Joel <fallendemongod@gmail.com>
2021-04-03 13:38:06 -0400

    Finished rest of functions for PlayerMovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -134,9 +134,210 @@ public class PlayerMovement : MonoBehaviour
         //Push player back up
         transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f,transform.position.z);
     }
-    
-    private void Movement()
-    
-    
 
+    private void Movement()
+    {
+        //Extra gravity to make sure player isn't flying
+        rb.AddForce(Vector3.down * Time.deltaTime * 10);
+        
+        //Find the actual velocity relative to where player is lookinhg
+        Vector2 mag = FindVelRelativeToLook();
+        
+        float xMag = mag.x, yMag = mag.y;
+        
+        
+        //Counteract sliding and sloppy movement
+        CounterMovement(x, y, mag);
+        
+        
+        //if Holding jump && ready to jump, then jump
+        if (readyToJump && jumping) Jump();
+        
+        //Set the max speed a player can go
+        float maxSpeed = this.maxSpeed;
+        
+        //if sliding down a ramp, add force down so the player stays on the ground but can also build up speed;
+        if (crouching && grounded && readyToJump)
+        {
+            //ADd a bunch of force downwards
+            rb.AddForce(Vector3.down * Time.deltaTime * 3000);
+            return;
+        }
+
+        
+        //if speed is larger then maxspeed, cancel out the input so you don't go over max speed
+        //If x > 0, and the magnitude of x is greater then the max speed, then set x to 0 so player can't surpass maxSpeed
+        if (x > 0 && xMag > maxSpeed) x = 0;
+        //If x < 0, which is negative speed and is LESS then the max speed, then set x to 0 so player can't go backward in time; 
+        if (x < 0 && xMag < -maxSpeed) x = 0;
+        
+        //Same as x but, replace x with Y. This is based on jump velocity.
+        if (y > 0 && yMag > maxSpeed) y = 0;
+        if (y < 0 && yMag < -maxSpeed) y = 0;
+        
+
+        //Multipliers to slow down movement/increase movement control
+        float multiplier = 1f, multiplierV = 1f;
+
+        //Movement in Air
+        if (!grounded)
+        {
+            multiplier = 0.5f;
+            multiplierV = 0.5f;
+        }
+
+        
+        //Movement while sliding
+        if (grounded && crouching) multiplierV = 0f;
+        
+        
+        //Apply all the forces generated to move player
+        rb.AddForce(orientation.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
+        rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
+    }
+
+
+    private void Jump()
+    {
+        //If player is ground and ready to jump
+        if (grounded && readyToJump)
+        {
+            readyToJump = false;
+            
+            //Add the jump forces
+            rb.AddForce(Vector2.up * jumpForce * 1.5f);
+            rb.AddForce(normalVector * jumpForce * 0.5f);
+            
+            //If Jumping while falling, reset y velocity
+            Vector3 vel = rb.velocity;
+            if (rb.velocity.y < 0.5f)
+            {
+                rb.velocity = new Vector3(vel.x, 0, vel.z);
+            }else if (rb.velocity.y > 0)
+                rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
+            
+            //Invoke ResetJump with the jumpCooldown
+            Invoke(nameof(ResetJump),jumpCooldown);
+        }
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private float desiredX;
+
+    private void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        
+        //Find current look rotation
+        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+        
+        //Rotate, and also make sure we don't over or under rotate
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90);
+        
+        //Perform the rotation of camera
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+        orientation.transform.localRotation = Quaternion.Euler(0,desiredX,0);
+    }
+
+    private void VounterMovement(float x, float y, Vector2 mag)
+    {
+        if (!grounded || jumping) return;
+        
+        //Slow down sliding
+        if (crouching)
+        {
+            rb.AddForce(moveSpeed * Time.deltaTime * -rb.velocity.normalized * slideCounterMovement);
+            return;
+        }
+        
+        //Counter movement
+        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) ||
+            (mag.x > threshold && x < 0))
+        {
+            rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
+        }  if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0)) {
+            rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
+        }
+        
+        //Limit diagonal running
+        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
+        {
+            float fallspeed = rb.velocity.y;
+            Vector3 n = rb.velocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(n.x, fallspeed, n.z);
+        }
+    }
+    
+    /// <summary>
+    /// Find the velocity relative to where the player is looking
+    /// Useful for vectors calculations regarding movement an dlmiting movement
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 FindVelRelativeToLook()
+    {
+        float lookAngle = orientation.transform.eulerAngles.y;
+        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+
+
+        float u = Mathf.DeltaAngle(lookAngle, moveAngle);
+        float v = 98 - u;
+
+        float magnitude = rb.velocity.magnitude;
+        float yMag = magnitude * Mathf.Cos(u * Mathf.Deg2Rad);
+        float xMag = magnitude * Mathf.Cos(v * Mathf.Deg2Rad);
+
+        return new Vector2(xMag, yMag);
+    }
+
+
+    private bool IsFloor(Vector3 v)
+    {
+        float angle = Vector3.Angle(Vector3.up, v);
+        return angle < maxSlopeAngle;
+    }
+
+    private bool cancellingGrounded;
+
+    private void OnCollisionStay(Collision other)
+    {
+        //Make sure we are only checking for walkable layers
+        int layer = other.gameObject.layer;
+        if (whatIsGround != (whatIsGround | (1 << layer))) return;
+        
+        //Iterate through every collision in a physics update
+
+        for (int i = 0; i < other.contactCount; i++)
+        {
+            Vector3 normal = other.contacts[i].normal;
+            
+            //Floor
+            if (IsFloor(normal))
+            {
+                grounded = true;
+                cancellingGrounded = false;
+                normalVector = normal;
+                CancelInvoke(nameof(StopGrounded));
+            }
+        }
+        
+        //Invoke ground/wall cancel, since we can't check normals with CollisionExit
+        float delay = 3f;
+        if (!cancellingGrounded)
+        {
+            cancellingGrounded = true;
+            Invoke(nameof(StopGrounded), Time.deltaTime * delay);
+        }
+    }
+
+    private void StopGrounded()
+    {
+        grounded = false;
+    }
 }
